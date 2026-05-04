5a2541ab269f453e65aa8e841bd992fca1fece6c
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-05-04 10:55:23 +0900

    Check tab.id existence before accessing it

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,14 @@
 browser.pageAction.onClicked.addListener(async (tab: browser.tabs.Tab) => {
+  if (tab.id == null) {
+    console.error("Tab ID is not available.");
+    return;
+  }
+
   await browser.scripting.executeScript({
     files: ["dist/content.js"],
-    target: { tabId: tab.id! },
+    target: { tabId: tab.id },
   });
-  const response = await browser.tabs.sendMessage(tab.id!, { action: "extractCanonicalURL" });
+  const response = await browser.tabs.sendMessage(tab.id, { action: "extractCanonicalURL" });
   const link = `[${tab.title}](${response.url || tab.url})`;
   await navigator.clipboard.writeText(link);
   console.log("Link has been copied:", link);
