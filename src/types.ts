4e40ab773b28309b60463ad97e985251ba1cdfdb
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-05-04 20:09:39 +0900

    Introduce ESLint

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 interface MessageReq {
   action: string;
 }
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 interface MessageRes {
   url: string | null;
 }
